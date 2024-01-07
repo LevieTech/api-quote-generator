9e8d7b1b0785fb8303c4c92f2d41e4753a787249
Sam Gossie <sjg3609@gmail.com>
2024-01-07 12:49:57 -0600

    Got the searchable quotes working by tags

--- src/redux/sagas/quote.saga.js
+++ src/redux/sagas/quote.saga.js
@@ -4,7 +4,8 @@ import { takeEvery, put} from 'redux-saga/effects';
 function* getQuote(action){
     try{
         console.log('quote saga console.log', action.payload);
-        const quotes = yield axios.post(`/api/search/quotes&query=${action.payload}`);
+        // const quotes = yield axios.post(`/api/search/quotes&query=${action.payload}`);
+        const quotes = yield axios.get(`/api/quotes/${action.payload}`);
         console.log('Checking quotes.data', quotes.data)
         yield put({ type: 'SET_QUOTES', payload: quotes.data });
 
