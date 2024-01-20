f5b6dd65f3a22db68198647b20684d3609ced629
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-01-19 19:40:08 -0500

    trying a search by author

---
+++ src/components/Quotes/SearchByAuthor.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { TextField, Button, Typography } from '@mui/material';
+
+function SearchByAuthor() {
+
+
+    return(
+        <div>
+
+            
+        </div>
+    )
+
+}// end searchbyauthor function
+
+export default SearchByAuthor;
