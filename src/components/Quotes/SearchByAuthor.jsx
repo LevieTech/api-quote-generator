accd69eaf398f5c196f34dec0b7f6b0386ea6c86
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-01-22 19:10:31 -0500

    component SearchByAuthor

--- src/components/Quotes/SearchByAuthor.jsx
+++ src/components/Quotes/SearchByAuthor.jsx
@@ -1,25 +1,29 @@
-// import React, { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { TextField, Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { TextField, Button, Typography } from '@mui/material';
 
-// function SearchByAuthor() {
-//     const [author, setAuthor] = useState('');
-//     const dispatch = useDispatch();
+function SearchByAuthor() {
+    const [author, setAuthor] = useState('');
+    const dispatch = useDispatch();
 
-// const handleSubmit = () => {
-//     // Dispatch an action to update the search query by author
-// //     dispatch({ type: 'SET_SEARCH_BY_AUTHOR', payload: author
-// // };
-// // )
-// }
+const handleSubmit = () => {
+    // Dispatch an action to update the search query by author
+    dispatch({ type: 'SET_SEARCH_BY_AUTHOR', payload: author });
+};
 
-//     return(
-//         <div>
+//Access results specifically by author from Redux state 
+const authorSearchResults = useSelector((state) => state.authorSearchResults);
+
+    return(
+        <div>
+            <Typography>
+                
+            </Typography>
 
 
-//         </div>
-//     )
+        </div>
+    )
 
-// }// end searchbyauthor function
+}// end searchbyauthor function
 
-// export default SearchByAuthor;
+export default SearchByAuthor;
--- src/redux/reducers/searchQuotes.reducer.js
+++ src/redux/reducers/searchQuotes.reducer.js
@@ -1,15 +1,17 @@
 const searchQuotes = (state = [], action) => {
     switch (action.type) {
         case 'SET_SEARCH_QUOTES':
-            return action.payload;
+            return { ...state, results: action.payload };
+            // return action.payload;
         // default:
         //     return state;
          
         case 'SET_SEARCH_BY_AUTHOR':
-            return action.payload;
-        default: 
-            return state;   
-    }
-}
+            // return action.payload;
+            return { ...state,author: action.payload, keyword: '', results: []}; 
+            default:
+                return state; 
+            }
+};
 
 export default searchQuotes;
