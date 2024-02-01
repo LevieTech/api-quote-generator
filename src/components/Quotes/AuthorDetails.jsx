f92779abbe2265e2ec53f6ef1a44c15b4702090c
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-02-01 18:47:25 -0500

    tried working on the authors details

--- src/components/Quotes/AuthorDetails.jsx
+++ src/components/Quotes/AuthorDetails.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useState } from 'react';
+// import { TextField, Button } from '@mui/material';
+
 function AuthorDetails() {
   const authorDetails = useSelector((state) => state.authorDetails); // Make sure to use "state" here
   console.log('Author details from Redux store:', authorDetails);
@@ -15,9 +19,23 @@ function AuthorDetails() {
     );
   }
 
+  //?Tried this 
+  // const [search, setSearch] = useState('');
+  // const dispatch = useDispatch();
+
+
+  // const handleSubmit = () => {
+  //     dispatch({ type: 'SET_SEARCH', payload: search })
+  //     dispatch({ type: 'SET_AUTHOR', payload: search }) // This is a dispatch to search by authors
+  // }
+
   return (
     <div className="author-details">
-
+//? tried this out 
+{/* <form onSubmit={handleSubmit}>
+                    <TextField fullWidth variant="standard" style={{ maxWidth: '25%', }} type="search" value={search} onChange={(event) => setSearch(event.target.value)} />
+                    <Button variant="contained" className="btn" type="submit">Submit</Button>
+                </form> */}
 
       {/* <h2>Author Details</h2> */}
      
